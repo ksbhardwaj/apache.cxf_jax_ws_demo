48bdcb91675e3e97afd389bef65e94bbbbb2c473
Ketan.Sharma <ketan.sharma@fdmgroup.com>
2017-09-06 11:28:23 -0400

    final commit

--- src/main/java/com/fdmgroup/apache/cxf_jax_ws_demo/HelloWorldImpl.java
+++ src/main/java/com/fdmgroup/apache/cxf_jax_ws_demo/HelloWorldImpl.java
@@ -3,11 +3,12 @@ package com.fdmgroup.apache.cxf_jax_ws_demo;
 
 import javax.jws.WebService;
 
-@WebService(endpointInterface = "com.fdmgroup.apache.cxf_jax_ws_demo.HelloWorld")
+@WebService(endpointInterface = "com.pluralsight.cxfdemo.HelloWorld")
 public class HelloWorldImpl implements HelloWorld {
 
     public String sayHi(String text) {
         return "Hello " + text;
     }
 }
+
 
